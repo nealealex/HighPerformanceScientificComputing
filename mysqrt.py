01ca8d9470075838dd908375baee9f34d7219693
Neale Alex Watson <nealewatson.com>
2013-08-09 19:43:57 +0100

    first proper commit

---
+++ mysqrt.py
@@ -0,0 +1,14 @@
+x =.01
+s=1.
+kmax = 100
+tol = 1.e-14
+for k in range(kmax):
+	print "Before iteration k %s, s = %s" % (k,s)
+	s0 = s
+	s = 0.5 * (s+x/s)
+	delta_s = s -s0
+	if abs(delta_s / x) < tol:
+		break
+print "After %s iterations, s = %s" % (k+1,s)
+
+ 
